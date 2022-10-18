0b3c1cb01c945cd1390ba3795adca7f85920a8c4
Martin <66073522+martinzizi@users.noreply.github.com>
2022-10-18 14:21:37 +0200

    update

--- sound.js
+++ sound.js
@@ -6,19 +6,11 @@ function playPause(){
 	if(count == 0){
 		count = 1;
 		audio.play();
-		playPauseBTN.innerHTML = "Pause ⏸";
 	}else{
 		count = 0;
 		audio.pause();
-		playPauseBTN.innerHTML = "Play ►";
+        audio.currentTime = 0;
 	}
 
-}
-
-function stop(){
-	playPause()
-	audio.pause();
-	audio.currentTime = 0;
-	playPauseBTN.innerHTML = "Play ►";
 }
 
